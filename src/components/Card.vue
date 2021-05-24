5318200aa01a3e11cc33c9657c84207ae15b6566
Kasidis Chaowvasin <seai_kasidis@hotmail.com>
2021-05-25 01:56:41 +0700

    finish desktop view

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">{{ title }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ['title']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ContactLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="is-flex is-align-items-center">
+    <a :href="link" target="_blank" class="is-flex is-align-items-center is-clickable">
+      <figure class="image is-32x32">
+        <img alt="" :src="getIconUrl(icon)">
+      </figure>
+      <p v-if="link" class="has-text-weight-semibold">{{ name }}</p>
+      <p v-else class="has-text-weight-semibold">{{ text }}</p>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ContactLink',
+  props: ['name', 'icon', 'link', 'text'],
+  methods: {
+    getIconUrl(name) {
+      return require(`@/assets/icons/${name}.png`)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  color: #fff;
+  &:hover {
+    p {
+      color: #fff;
+    }
+  }
+}
+</style>
